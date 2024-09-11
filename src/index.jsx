c75dac3ac409ae49497a10528d1cb6e14cb46083
FrozenHearth <vishudbz@gmail.com>
2024-09-11 20:57:30 +0530

    Tests + build pipeline

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./styles.css";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('app')).render(
+ReactDOM.createRoot(document.getElementById("app")).render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
   </Router>
-)
+);
